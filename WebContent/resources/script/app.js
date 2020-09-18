9186867fd9f6017b70ecc2f62a2e14567cb378e4
francislukose <francislukose@gmail.com>
2020-09-18 14:45:44 -0500

    Search functionality

--- WebContent/resources/script/app.js
+++ WebContent/resources/script/app.js
@@ -15,3 +15,15 @@ function selectListItem(e) {
 	$('.sidebar-list-group-item').removeClass("active");
 	$(e).addClass("active");
 }
+
+function searchInList(e,l) {
+	var filter = $(e).val().toUpperCase();
+	$('#' + l).children().each(function(index){
+		var textValue =  $(this).text();
+		if (textValue.toUpperCase().indexOf(filter) > -1) {
+	      	$(this).css("display", "");
+	    } else {
+	      	$(this).css("display", "none");
+	    }
+	});
+}
